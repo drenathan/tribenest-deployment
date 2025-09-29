7d9634e229e3a099cc343ec67a88adccfc7ce2fa
oludare odedoyin <damiflo94@gmail.com>
2025-09-30 01:52:01 +0200

    update nginx

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -152,6 +152,11 @@ server {
         proxy_connect_timeout 60s;
         proxy_send_timeout 60s;
         proxy_read_timeout 60s;
+        fastcgi_buffers 16 16k;
+        fastcgi_buffer_size 32k;
+        proxy_buffer_size   128k;
+        proxy_buffers   4 256k;
+        proxy_busy_buffers_size   256k;
     }
 
     location /health {
